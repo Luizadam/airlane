8a487d15a9c3014475173c13fc3c48077d4d2419
Luizadam <luizadaam@gmail.com>
2020-07-01 23:16:19 +0700

    digit confirmation input and make sure css

---
+++ assets/js/digit.js
@@ -0,0 +1,25 @@
+function getCodeBoxElement(index) {
+    return document.getElementById('digit' + index);
+  }
+  function onKeyUpEvent(index, event) {
+    const eventCode = event.which || event.keyCode;
+    if (getCodeBoxElement(index).value.length === 1) {
+      if (index !== 4) {
+        getCodeBoxElement(index+ 1).focus();
+      } else {
+        getCodeBoxElement(index).blur();
+      }
+    }
+    if (eventCode === 8 && index !== 1) {
+      getCodeBoxElement(index - 1).focus();
+    }
+  }
+  function onFocusEvent(index) {
+    for (item = 1; item < index; item++) {
+      const currentElement = getCodeBoxElement(item);
+      if (!currentElement.value) {
+          currentElement.focus();
+          break;
+      }
+    }
+  }
